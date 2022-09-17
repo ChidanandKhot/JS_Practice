686444cd7ff0c60fa4f8d22f297c5a3d356342ca
chidanandkhot <chidukhot@gmail.com>
2022-09-17 13:15:37 +0530

    Js basic

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Am on console");
+console.log(25+6);
+console.log(5*2);
+console.log(5**2);
+console.log([25,24,20,15,3,45]);
+console.log({object1:"Manu", Age:25, Marks : 55, place:"Blore"});
+console.table({object1:"Manu", Age:25, Marks : 55, place:"Blore"});
+// console.error("this is my error");
+ /* test for multiline comment 
+ am just trying 
+ bcoz its multiline */
+ 
