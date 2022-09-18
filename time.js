7b80977985c2cffca6d5aab87e465b2e86dd507b
chidanandkhot <chidukhot@gmail.com>
2022-09-19 00:51:23 +0530

    object Binding Practice

---
+++ time.js
@@ -0,0 +1,24 @@
+
+
+let a= 100;
+
+function funClick(){
+    const date = Date();
+    // console.log (date.getFullYear());
+    document.getElementById("print").innerHTML=date;
+
+    const funClick2 = setInterval(funClick, 1000);
+
+    //  function stop(){
+        // date.stopInterval(funClick,);
+
+    }
+
+    const stopInterval = stop() 
+    {
+        clearInterval(funClick2);
+    document.getElementById("print").innerHTML=stop();
+
+    }
+
+// }
